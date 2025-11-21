ff55158f43d803cdefcdebdea5d7217649ca6024
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-11-21 15:16:30 +0000

    fix leftover issue from dev

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import {router as adminRouter} from "./admin.ts";
-import {router as authRouter} from "./auth.ts";
+import { router as adminRouter } from "./admin.ts";
+import { router as authRouter } from "./auth.ts";
 import cookieParser from "cookie-parser";
-import {router as webRouter} from "./web.ts";
-import {router as appRouter} from "./app.ts";
+import { router as webRouter } from "./web.ts";
+import { router as appRouter } from "./app.ts";
 import https from "https";
 import fs from "fs";
 
@@ -16,13 +16,13 @@ app.use("/admin", adminRouter);
 app.use("/app", appRouter);
 app.use("/static", express.static("./server/web/static"));
 
-// app.listen(7045, () => {
-//     console.log("Server started on port 7045");
-// });
-
-https.createServer({
-    key: fs.readFileSync("./https/key.key"),
-    cert: fs.readFileSync("./https/cert.crt")
-}, app).listen(7045, () => {
+app.listen(7045, () => {
     console.log("Server started on port 7045");
 });
+
+// https.createServer({
+//     key: fs.readFileSync("./https/key.key"),
+//     cert: fs.readFileSync("./https/cert.crt")
+// }, app).listen(7045, () => {
+//     console.log("Server started on port 7045");
+// });
