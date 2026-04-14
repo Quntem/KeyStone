092578200777ba8fd560d6e05305399c248847d1
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2026-04-14 14:26:40 +0100

    hotfix: HTTPS certificate

--- server/index.ts
+++ server/index.ts
@@ -21,8 +21,8 @@ app.use("/static", express.static("./server/web/static"));
 // });
 
 https.createServer({
-    key: fs.readFileSync("./https/key.key"),
-    cert: fs.readFileSync("./https/cert.crt")
+    // key: fs.readFileSync("./https/key.key"),
+    // cert: fs.readFileSync("./https/cert.crt")
 }, app).listen(7045, () => {
     console.log("Server started on port 7045");
 });
